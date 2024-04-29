657598610ddedc908782537e1d10c16624a5887e
Gadesahaja <sahajagade@gmail.com>
2024-04-29 22:23:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import WeatherApp from './components/weatherApp/weatherApp'
+
+function App() {
+
+  return (
+    <>
+      <WeatherApp/>
+    </>
+  )
+}
+
+export default App
